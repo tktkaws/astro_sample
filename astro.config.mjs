a4d9684670ba09c2203df92ba179bd0edc3cb027
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-03-29 20:42:14 +0900

    chapter1 done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import { siteMeta } from './src/lib/constants';
+const { siteUrl } = siteMeta;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: siteUrl,
+});
