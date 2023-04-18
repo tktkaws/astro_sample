9fe24d71a26194d89ef7b2381e94a63b9072f7c3
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-04-18 20:37:26 +0900

    chapter2 記事の追加と一覧の作成

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,14 +1,14 @@
 export const siteMeta = {
-	siteTitle: 'Tech Flow.',
-	siteDesc: 'Tech Flowの技術ブログサイト',
-	siteUrl: 'https://*********',
-	siteLocale: 'ja_JP',
-	siteType: 'website',
-	siteIcon: '/favicon.png',
-	siteImg: '/ogp.jpg',
+  siteTitle: "Tech Flow.",
+  siteDesc: "Tech Flowの技術ブログサイト",
+  siteUrl: "https://*********",
+  siteLocale: "ja_JP",
+  siteType: "website",
+  siteIcon: "/favicon.png",
+  siteImg: "/ogp.jpg",
 };
 
-// export const categories = [
-// 	{ categoryName: 'Python', categorySlug: 'python' },
-// 	{ categoryName: 'JavaScript', categorySlug: 'javascript' },
-// ];
+export const categories = [
+  { categoryName: "Python", categorySlug: "python" },
+  { categoryName: "JavaScript", categorySlug: "javascript" },
+];
