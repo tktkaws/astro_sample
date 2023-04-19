02cbd624376d4cb5443afea5342067daa31bfe82
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-04-19 20:13:29 +0900

    chapter4 記事ページの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import { siteMeta } from "./src/lib/constants";
 import image from "@astrojs/image";
-const { siteUrl } = siteMeta;
+import mdx from "@astrojs/mdx";
+const {
+  siteUrl
+} = siteMeta;
+
 
 // https://astro.build/config
 export default defineConfig({
   site: siteUrl,
-  integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx()]
 });
