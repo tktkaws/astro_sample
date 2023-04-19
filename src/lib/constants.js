adbca376442d88cfea24f81540801be394e80008
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-04-19 20:25:47 +0900

    chapter5 サイトの仕上げ

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,7 +1,7 @@
 export const siteMeta = {
   siteTitle: "Tech Flow.",
   siteDesc: "Tech Flowの技術ブログサイト",
-  siteUrl: "https://*********",
+  siteUrl: "https://astro-sample-umber.vercel.app/",
   siteLocale: "ja_JP",
   siteType: "website",
   siteIcon: "/favicon.png",
